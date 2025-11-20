71e8c4a04827674878100ae9c1126b468f6af3f3
AlexandreDeCamargo <decamara@mcmaster.ca>
2025-11-20 16:05:01 -0500

    updating

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages
 
-# Parse requirements from file
-with open('requirements.txt', 'r') as f:
-    requirements = f.read().splitlines()
 
 short_description = \
     """
@@ -20,16 +17,9 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/AlexandreDeCamargo/of_flows',
     packages=find_packages(),
-    install_requires=requirements,
-    extras_require={
-        "examples": [
-            "matplotlib>=3.10.0", "pandas>=2.0.3", "chex>=0.1.89"
-        ],
-    },  
     classifiers=[
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research"
-        "License :: OSI Approved ::  MIT License",
         "Programming Language :: Python :: 3.11",
     ],
 )
